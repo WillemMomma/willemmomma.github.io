915d6ca50097b80b0d31accb7d1c1d3a94806224
WillemMomma <willemmomma@gmail.com>
2024-05-05 13:11:15 +0200

    initial styling webpage

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+document.addEventListener('mousemove', function (event) {
+    const glow = document.createElement('div');
+    glow.className = 'glow';
+    glow.style.left = `${event.pageX}px`;
+    glow.style.top = `${event.pageY}px`;
+    document.body.appendChild(glow);
+
+    setTimeout(() => {
+        glow.remove();
+    }, 1000); // Removes after 1 second
+});
+
